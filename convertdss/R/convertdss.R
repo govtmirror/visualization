329e259215a0e719d6797938dee76bee75db732a
cbracken-usbr <cbracken@usbr.gov>
2015-03-31 19:05:09 -0600

    Can write a single variable to a netcdf file now

--- convertdss/R/convertdss.R
+++ convertdss/R/convertdss.R
@@ -1,31 +1,89 @@
-#' dss_to_list get data from a dss file as a list of lists 
+#' function  
 #' 
-#' read dss data
+#' Writes a dss variable to an existing netcdf file
+#' 
+#' Uses the output of read_dss_variable, creating a new variable in an _existing_
+#' netcdf file
+#' 
+#' @return new netcdf file handle
+#' @note NOTE
+#' @author Cameron Bracken
+#' @export 
+dss_var_to_ncdf <- function(v, nc){
+
+    # Define a time dimension 
+    time_dim = ncdim_def('time','Time indicies', 1:nrow(v$data), unlim=T)
+    var_name = v$variable
+    md = lapply(v$metadata,as.character)
+
+    #browser()
+    # define the variable, with no data
+    var = ncvar_def(var_name, md$units, list(time_dim), missval = NA)
+
+    # Add the variable to an existing nc file, creates the data container
+    nc2 = ncvar_add(nc, var)
+
+    # Write the data
+    ncvar_put(nc2, var, v$data$value)
+
+    # add the metadata
+    for(m in names(md))
+        ncatt_put(nc2, var_name, m, as.character(md[[m]]))
+    
+    #nc_close(nc)
+
+    return(nc2)
+
+}
+
+#' function  
+#' 
+#' short description
 #' 
 #' Long Description
 #' 
 #' @return list of lists containing dss data
 #' @note NOTE
-#' @author Evan Heisman
+#' @author Cameron Bracken
+#' @export 
+dss_to_netcdf <- function(){
+
+
+
+}
+
+#' dss_to_list get data from a dss file as a list of lists 
+#' 
+#' read dss data into a list
+#' 
+#' Long Description
+#' 
+#' @return list of lists containing dss data
+#' @note NOTE
+#' @author Cameron Bracken
 #' @export 
 read_dss_list <- function(dss,variable_part = 'B',time_part = 'D'){
-    # open the DSS file and fiddle with the paths to allow bulk extraction of a 
-    # single variable. Internally, DSS stores monthly data in 10 year chunks (why?!?!)
-    #require(parallel)
-    #require(doParallel)
-    require(data.table)
-
     #registerDoParallel(makeCluster(detectCores()))
 
     paths = getAllPaths(dss)
     parts = data.table(separatePathParts(paths))
 
-    d = dlply(parts[1:100],.(get(variable_part)), summarise, 
+    d = dlply(parts,.(get(variable_part)), summarise, 
         read_dss_variable(get(variable_part)[1],PATH,dss))#,.parallel=TRUE)
         
     lapply(d,'[[',1)
 }
 
+#' function  
+#' 
+#' short description
+#' 
+#' Long Description
+#' 
+#' @return list of lists containing dss data
+#' @note NOTE
+#' @author Cameron Bracken
+#' @export 
 read_dss_variable <- function(variable,path_group,dss){
 
     # get a single variable as a data.table
@@ -34,7 +92,7 @@ read_dss_variable <- function(variable,path_group,dss){
 
     metadata = attr(dt,'dssMetadata')[1,]
     attr(dt,'dssMetadata') = NULL
-    return(list(data=dt,metadata=metadata))
+    return(list(data=dt,metadata=metadata,variable=variable))
 }
 
 # Determine if range of vector is FP 0.
